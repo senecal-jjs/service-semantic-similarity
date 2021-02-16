464f475f9e96914ca50cf72c64a18a81408de0e0
Jacob Senecal <jsenecal@figure.com>
2021-02-15 22:41:40 -0700

    cleanup

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,11 +2,7 @@ from app.api import bp
 from app import db
 from app.models import RssChannel
 from app.lsi import WMD
-import gensim.downloader as api
-from sqlalchemy import inspect 
 
-
-# word_vectors = api.load("glove-wiki-gigaword-100")
 wmd = WMD.WMD()
 model = wmd.get_model()
 
@@ -22,6 +18,4 @@ def search_feeds(searchTerm):
         top_score = min([desc_score] + topic_scores)
         channel_scores[f"{c.id}"] = top_score
 
-    # print(channels.channel_url)
-    # print(channels.topics['topics'])
     return(channel_scores)
